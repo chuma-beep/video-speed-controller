337cf9d87d345bea1803d45038e31b94d0336237
chuma-beep <wisboynelson123@gmail.com>
2023-09-03 05:40:32 +0100

    new dditions

---
+++ app.js
@@ -0,0 +1,10 @@
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+
+
+
+
+speed.addEventListener('mousemove', function() {
+    console.log(e);
+})
